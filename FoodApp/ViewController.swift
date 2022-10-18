d223bc6051357cee073b5f46a60237b3ed020cf9
ErtasBurak <burakertas23@hotmail.com>
2022-10-18 13:30:35 +0300

    initial commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Burak Ertaş on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
